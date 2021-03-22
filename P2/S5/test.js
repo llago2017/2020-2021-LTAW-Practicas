3a536f641fee5110850102f505ff46b7425cb485
llago2017 <l.lago.2017@alumnos.urjc.es>
2021-03-22 19:47:30 +0100

    accediendo a valores json

--- P2/S5/test.js
+++ P2/S5/test.js
@@ -14,3 +14,10 @@ const tienda = JSON.parse(tienda_json);
 //-- Muestro info
 console.log("Usuarios en la tienda: " + tienda["usuarios"].length);
 console.log("Productos en la tienda: " + tienda["productos"].length);
+
+//-- Cambio un valor
+console.log("Primer usuario: " + tienda["usuarios"][0]["nombre"]);
+
+tienda["usuarios"][0]["nombre"] = "cambio";
+
+console.log("Primer usuario cambiado: " + tienda["usuarios"][0]["nombre"]);
