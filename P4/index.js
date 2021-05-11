830b1967c3f21b7561a4f8716da923c13f32781a
llago2017 <lago.2017@alumnos.urjc.es>
2021-05-11 16:52:06 +0200

    Prueba chat con electron

--- P4/index.js
+++ P4/index.js
@@ -2,10 +2,10 @@ console.log("Hola desde el proceso de la web...");
 
 //-- Obtener elementos de la interfaz
 const btn_test = document.getElementById("btn_test");
-const display = document.getElementById("display");
 const info1 = document.getElementById("info1");
 const info2 = document.getElementById("info2");
 const info3 = document.getElementById("info3");
+const info4 = document.getElementById("info4");
 
 //-- Acceder a la API de node para obtener la info
 //-- Sólo es posible si nos han dado permisos desde
@@ -13,9 +13,95 @@ const info3 = document.getElementById("info3");
 info1.textContent = process.version;
 info2.textContent = process.versions["electron"]
 info3.textContent = process.versions["chrome"]
+info4.textContent = "https://127.0.0.1.8080/Ej-09.html"
 
 console.log(process.versions)
 btn_test.onclick = () => {
     display.innerHTML += "TEST! ";
     console.log("Botón apretado!");
+}
+
+// CHAT
+
+//-- Elementos del interfaz
+const display = document.getElementById("display");
+const msg_entry = document.getElementById("msg_entry");
+const login_msg = document.getElementById("user");
+const login_backgr = document.getElementsByClassName("chat");
+var elmnt = document.getElementById("msg_recibidos");
+var typing_msg = document.getElementsByClassName("typing");
+var user = "";
+var typing = false;
+var nickname = false;
+
+//-- Crear un websocket. Se establece la conexión con el servidor
+const socket = io();
+
+
+function main() {
+  // Cada 100ms se actualiza el scroll para estar actualizado
+  setInterval(function(){  
+    elmnt.scrollTop = elmnt.scrollHeight;
+  }, 100);
+
+  document.onkeydown = function(ev) {
+    console.log("Pulsando trcals")
+    onKeyDown();
+  }
+
+  document.onkeyup = function(ev) {
+    console.log("Sa parap")
+    typingstopped();
+  }
+}
+
+
+function typingstopped(){
+  typing = false;
+  //socket.emit(notTyping);
+}
+
+function onKeyDown(){
+  if(typing == false) {
+    typing = true
+    console.log(user)
+    socket.emit('typing', user );
+    time = setTimeout(typingstopped, 500);
+  } else {
+    clear(time);
+    time = setTimeout(typingstopped, 500);
+  }
+
+}
+
+socket.on('display', (data) => {
+  console.log('HA LLEGAOS')
+  typing_msg.innerHTML = data + " is typing";
+  typing_msg[0].style.display = 'block'
+});
+
+socket.on("message", (msg)=>{
+  display.innerHTML += '<p style="color:blue">' + msg + '</p>';  
+});
+
+//-- Al apretar el botón se envía un mensaje al servidor
+msg_entry.onchange = () => {
+  if (nickname) {
+    if (msg_entry.value)
+    socket.send(msg_entry.value);
+  
+    //-- Borrar el mensaje actual
+    msg_entry.value = "";
+  } else {
+    socket.emit('nickname',msg_entry.value);
+    user = msg_entry.value;
+    msg_entry.value = "";
+    login_msg.style.display = 'none'
+    // El fondo pasa a blanco
+    login_backgr[0].style.animationName = 'static'
+    login_backgr[0].style.textAlign = "left"
+    nickname = true;
+  }
+
+  
 }
--- P4/main.js
+++ P4/main.js
@@ -23,17 +23,140 @@ electron.app.on('ready', () => {
           contextIsolation: false
         }
     });
-
-  //-- En la parte superior se nos ha creado el menu
-  //-- por defecto
-  //-- Si lo queremos quitar, hay que añadir esta línea
-  //win.setMenuBarVisibility(false)
-
-  //-- Cargar contenido web en la ventana
-  //-- La ventana es en realidad.... ¡un navegador!
-  //win.loadURL('https://www.urjc.es/etsit');
+  // Quito menú por defecto
+  win.setMenuBarVisibility(false)
 
   //-- Cargar interfaz gráfica en HTML
   win.loadFile("index.html");
 
 });
+
+// TODO CHAT
+//-- Cargar las dependencias
+const socket = require('socket.io');
+const http = require('http');
+const express = require('express');
+const colors = require('colors');
+
+// Control de número de usuarios
+var users = 0;
+var dict = [];
+
+const PUERTO = 8080;
+
+//-- Crear una nueva aplciacion web
+const app = express();
+
+//-- Crear un servidor, asosiaco a la App de express
+const server = http.Server(app);
+
+//-- Crear el servidor de websockets, asociado al servidor http
+const io = socket(server);
+
+//-------- PUNTOS DE ENTRADA DE LA APLICACION WEB
+//-- Definir el punto de entrada principal de mi aplicación web
+app.get('/', (req, res) => {
+  res.send('Bienvenido a mi aplicación Web!!!' + '<p><a href="/Ej-09.html">Test</a></p>');
+});
+
+//-- Esto es necesario para que el servidor le envíe al cliente la
+//-- biblioteca socket.io para el cliente
+app.use('/', express.static(__dirname +'/'));
+
+//-- El directorio publico contiene ficheros estáticos
+app.use(express.static('public'));
+
+//------------------- GESTION SOCKETS IO
+//-- Evento: Nueva conexion recibida
+io.on('connect', (socket) => {
+  
+  console.log('** NUEVA CONEXIÓN **'.yellow);
+
+  // Obtengo el nombre de usuario
+  socket.on("nickname", (nickname) => {
+    console.log('Nombre de usuario: ' + nickname.red)
+    welcome_msg = nickname + ' se ha unido al chat!'
+    socket.username = nickname;
+    dict.push({ name: socket.username, id: socket.id });
+    io.send(welcome_msg);
+  });
+  users += 1;
+
+  //-- Evento de desconexión
+  socket.on('disconnect', function(){
+    console.log('** CONEXIÓN TERMINADA **'.yellow);
+    users -= 1;
+  });  
+
+  //-- Evento de escribiendo
+  socket.on('typing', (data) => {
+      io.emit('display', data)
+  });  
+
+  //-- Mensaje recibido: Reenviarlo a todos los clientes conectados
+  socket.on("message", (msg)=> {
+    console.log("Mensaje Recibido!: " + msg.blue);
+    var socketId = socket.id;
+    console.log("socket id: " + socketId.green + " " +socket.username);
+
+    if (msg=='/help') {
+        console.log("Mensaje de ayuda".red)
+        msg = '/help: Devuelve la lista con todos los comandos' + "<br>" +
+        '/list : Devvuelve el numero de usuarios conectados' + "<br" +
+        '/hello : Devuelve el saludo del servidor' + "<br>" +
+        '/date : Devulve la fecha'
+
+        io.to(socketId).emit('message', msg);
+    } else if (msg == '/hello'){
+        console.log("Mensaje de saludo".red)
+        msg = '¡HOLI!';
+        console.log(socketId);
+        io.to(socketId).emit('message', msg);
+    }  else if (msg == '/date'){
+        var d = new Date();
+        var yy = d.getFullYear();
+        var mm = d.getMonth();
+        var dd = d.getDate();
+        msg = 'Fecha: ' + dd + '/' + mm + '/' + yy;
+        io.to(socketId).emit('message', msg);
+    } else if (msg == '/list') {
+      msg = 'Actualmente hay ' + users + " usuarios conectados. <br>"
+      console.log(dict)
+      // Recorro el diccionario de usuarios
+      for (i=0; i< dict.length; i++){
+        msg += "> " + (dict[i].name) + "<br>"
+      }
+      io.to(socketId).emit('message', msg);
+    } else if (msg.split("/")[0] == "") {
+      // msg.split("/")[0] --> " "
+      // msgsplit("/")[1] --> "username + mensaje"
+      console.log("Es un mensaje privado")
+
+      // separo el usuario del mensaje
+      var index = msg.split("/")[1].indexOf(" ")
+      var new_msg = msg.slice(index + 1)
+      var user2priv = msg.split("/")[1].slice(0,index)
+
+      for (i=0; i< dict.length; i++){
+        if (dict[i].name == user2priv) {
+          var priv_id = dict[i].id;
+          break;
+        }
+      }
+        console.log("MENSAJE A: " + user2priv.red + " con id: " + priv_id.green)
+       // Me quedo solo con el mensaje haciendo un split [0] --> /username
+       // [1] --> mensaje 
+        io.to(priv_id).emit('message', socket.username + ": " + new_msg);
+        io.to(socketId).emit('message', socket.username + ": " + new_msg);
+    } else {
+      //-- Reenviarlo a todos los clientes conectados
+          io.send(socket.username + ": " + msg);
+    }
+  });
+
+});
+
+//-- Lanzar el servidor HTTP
+//-- ¡Que empiecen los juegos de los WebSockets!
+server.listen(PUERTO);
+console.log("Escuchando en puerto: " + PUERTO);
