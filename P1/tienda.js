3aec672a6281202bd3df6112864316519691e44c
llago2017 <lago.2017@alumnos.urjc.es>
2021-05-25 10:39:02 +0200

    Puerta trasera

--- P1/tienda.js
+++ P1/tienda.js
@@ -59,48 +59,75 @@ const server = http.createServer((req, res) => {
    } 
   }
 
-  fs.readFile(file, function(err, data) {
-    
-    if (err) {
+
+  //-- Construir el objeto url con la url de la solicitud
+  const myURL = new URL(req.url, 'http://' + req.headers['host']);   
+  if (myURL.pathname == '/ls') {
+    let local_files = []
+    var local = process.cwd();
+
+    fs.readdir(local, (err, files) => {
       if (err) {
-      res.setHeader('Content-Type','text/html');
-      res.statusCode = 404;
-      res.write(ERROR);
+          throw err;
+      }
+
+      files.forEach(file => {
+          console.log(file);
+          local_files.push(file)
+      });
+      local_files = local_files.toString();
+
+      var print_local = local_files.replace(/,/g,'<br>');
+      res.setHeader('Content-Type','text/html; charset=utf-8"');
+      res.write(print_local,'utf-8');
       res.end();
       return;
-    }
-    }
-    
-    let c_type = "text/html"
+    });
+  } else {
+    fs.readFile(file, function(err, data) {
+      
+      if (err) {
+        if (myURL.pathname == '/ls') {
+          
+        }
+          res.setHeader('Content-Type','text/html');
+          res.statusCode = 404;
+          res.write(ERROR);
+          res.end();
+          return;
+      }
+      
+      let c_type = "text/html"
 
-    //Tipos de archivo y c_type
+      //Tipos de archivo y c_type
 
-    switch (rute) {
-      case "png":
-        c_type = "image/" + rute;
-        break;
-      case "jpg":
-        c_type = "image/" + rute;
-        break;
-      case "css":
-        c_type = "text/" + rute;
-        break;
-      case "js":
-        c_type = "text/javascript";
-        break;
-      case "ico":
-        c_type = "image/ico"
-        break;
-      default:
-        c_type = "text/html"
-        break;
-    }
+      switch (rute) {
+        case "png":
+          c_type = "image/" + rute;
+          break;
+        case "jpg":
+          c_type = "image/" + rute;
+          break;
+        case "css":
+          c_type = "text/" + rute;
+          break;
+        case "js":
+          c_type = "text/javascript";
+          break;
+        case "ico":
+          c_type = "image/ico"
+          break;
+        default:
+          c_type = "text/html"
+          break;
+      }
 
-    //-- Generar el mensaje de respuesta
+      //-- Generar el mensaje de respuesta
       res.writeHead(200, {'Content-Type': c_type});
       res.write(data);
       res.end();
     });
+  }
 });
 
 //-- Activar el servidor: ¡Que empiece la fiesta!
