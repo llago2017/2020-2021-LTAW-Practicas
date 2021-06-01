ebbbca61fa088cafd98270808e94e6b5c176da02
llago2017 <l.lago.2017@alumnos.urjc.es>
2021-06-01 13:48:17 +0200

    Cambio de nombre

--- P4/main.js
+++ P4/main.js
@@ -77,7 +77,7 @@ const io = socket(server);
 //-------- PUNTOS DE ENTRADA DE LA APLICACION WEB
 //-- Definir el punto de entrada principal de mi aplicación web
 app.get('/', (req, res) => {
-  res.send('Bienvenido a mi aplicación Web!!!' + '<p><a href="/Ej-09.html">Test</a></p>');
+  res.send('Bienvenido a mi aplicación Web!!!' + '<p><a href="/client.html">Test</a></p>');
 });
 
 //-- Esto es necesario para que el servidor le envíe al cliente la
